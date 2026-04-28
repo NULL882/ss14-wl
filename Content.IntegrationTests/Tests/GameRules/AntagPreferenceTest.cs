fb0fdd1a51732cd87e49249913936769e9b94e84
Charlotte Tezuka <tau271@proton.me>
2026-04-28 19:03:27 +0200

    Upstream jobtest fix

--- Content.IntegrationTests/Tests/GameRules/AntagPreferenceTest.cs
+++ Content.IntegrationTests/Tests/GameRules/AntagPreferenceTest.cs
@@ -1,6 +1,4 @@
 #nullable enable
-using System.Collections.Generic;
-using System.Linq;
 using Content.IntegrationTests.Fixtures;
 using Content.Server.Antag;
 using Content.Server.Antag.Components;
